f911292fedb7de5531108fa81006e710138fe98a
Salah Tawafsha <93351227+SalahTawafsha@users.noreply.github.com>
2023-07-18 21:40:11 +0300

    v1

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pytsk3
 
+
+# Function to check if a file has a specific signature
 def has_signature(file_entry):
     file_data = file_entry.read_random(0, len(signature))
 
     return file_data == signature
 
 
+# Function to save a file to disk
 def save_file(file_entry, output_dir):
     file_size = file_entry.info.meta.size
     file_data = file_entry.read_random(0, file_size)
@@ -15,24 +18,29 @@ def save_file(file_entry, output_dir):
         output_file.write(file_data)
 
 
-image_file_path = "D:\\folder_image\\forensic.001"
+ad1_file_path = "D:\\folder_image\\forensic.001"  # Replace with the actual path to the .ad1 file
 
-image_handle = pytsk3.Img_Info(image_file_path)
+# Open the .ad1 file as a handle
+ad1_handle = pytsk3.Img_Info(ad1_file_path)
 
-image_fs = pytsk3.FS_Info(image_handle)
+# Open the .ad1 file system
+ad1_fs = pytsk3.FS_Info(ad1_handle)
 
 signature = b'\x50\x4B\x03\x04'
 
+# Output directory to save the matching files
 output_directory = 'C:\\Users\\user\\PycharmProjects\\forensic\\docs'
 
-root_directory = image_fs.open_dir(path='/')
+# Get the root directory of the .ad1 file system
+root_directory = ad1_fs.open_dir(path='/')
 
+# List all files and directories in the root directory
 for entry in root_directory:
-
+    # print entry name
     if hasattr(entry.info.meta,
                "type") and entry.info.meta.type == pytsk3.TSK_FS_META_TYPE_REG and entry.info.meta.size > 0:
-
+        # Check if the file has the desired signature
         if has_signature(entry):
-
+            # Save the matching file to disk
             save_file(entry, output_directory)
-            print(f'DOCX found and saved in: docx/{str(entry.info.name.name)[2:-1]}')
+            print(f'PDF found and saved in: docs/{str(entry.info.name.name)[2:-1]}')
